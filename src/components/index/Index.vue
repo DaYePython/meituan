9fa3ff5b1192910d1701fa4d0f872727d3387de0
DaYePython <yesongsheng1984240775@gmail.com>
2018-11-30 22:05:34 +0800

    init project

---
+++ src/components/index/Index.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="index">
+        <div class="wrapper">
+            <mt-swipe
+                :auto="0"
+                v-for="item of swipeData"
+                :key="item.id"
+            >
+                <mt-swipe-item>
+                    <img :src="item.pic">
+                </mt-swipe-item>
+            </mt-swipe>
+        </div>
+
+        <div class="types">
+            <type-item
+                v-for="item in types"
+                :key="item.id"
+                :ico="item.ico"
+                :txt="item.txt"
+                @toList="toListHandle(item)"
+            > </type-item>
+        </div>
+        分割线
+        <cross-line></cross-line>
+    </div>
+</template>
+
+<script>
+    import TypeItem from 'base/typeitem/TypeItem'
+    import CrossLine from 'base/crossline/CrossLine'
+    export default {
+        name: 'Index',
+        components: {
+            TypeItem,
+            CrossLine
+        },
+        methods: {
+            toListHandle(item){
+                // console.log(item.txt)
+                // TODO restaurnt_list
+                console.log("I want to gt to " + item.txt)
+            }
+        },
+        data () {
+            return {
+                swipeData: [
+                    {
+                        id: "000",
+                        pic: require('./img/swipe/bannertemp.e8a6fa63.jpg')
+                    }
+                ],
+                types: [
+                    {
+                        id: "002",
+                        ico: require('./img/types/types (7).png'),
+                        txt: '美食'
+                    },
+                    {
+                        id: "003",
+                        ico: require('./img/types/types (0).png'),
+                        txt: '美团超市'
+                    },
+                    {
+                        id: "004",
+                        ico: require('./img/types/types (1).png'),
+                        txt: '生鲜果蔬'
+                    },
+                    {
+                        id: "005",
+                        ico: require('./img/types/types (5).png'),
+                        txt: '甜点饮品'
+                    },
+                    {
+                        id: "006",
+                        ico: require('./img/types/types (4).png'),
+                        txt: '正餐优选'
+                    },
+                    {
+                        id: "007",
+                        ico: require('./img/types/types (2).png'),
+                        txt: '美团专送'
+                    },
+                    {
+                        id: "008",
+                        ico: require('./img/types/types (3).png'),
+                        txt: '能量西餐'
+                    },
+                    {
+                        id: "009",
+                        ico: require('./img/types/types (6).png'),
+                        txt: '精品小吃'
+                    }
+                ],
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper
+        overflow hidden
+        height 3.4rem
+        font-size .6rem
+        text-align center
+        img
+            width 100%
+            height 100%
+    .types
+        overflow hidden
+        padding-bottom .4rem
+        background-color #fff
+    .corss-line
+        background-color #ebebeb
+        width 100%
+        height .32rem
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+// 全局插件
+import router from './router'
+import fastClick from 'fastclick'
+import Mint from 'mint-ui'
+import 'babel-polyfill'
+import VueLazyload from 'vue-lazyload'
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// 全局css文件
+import 'mint-ui/lib/style.css'
+import 'style/border.css'
+import 'style/reset.css'
+
+Vue.config.productionTip = false
+
+// 应用插件
+fastClick.attach(document.body)
+Vue.use(Mint)
+Vue.use(VueLazyload, {
+  loading: '/static/loading-bars.svg'
+})
+// Vue.use(VueAwesomeSwiper)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
